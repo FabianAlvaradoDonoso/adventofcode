eb421896a4d7a7dd15f956b7c027a166405d9004
FabianAlvaradoDonoso <fabian.alvarado1008@gmail.com>
2024-10-30 08:48:33 -0300

    Add solution for 2015 Day 3: Perfectly Spherical Houses in a Vacuum

--- 2015/solutions/day03.py
+++ 2015/solutions/day03.py
@@ -2,8 +2,41 @@ from utils.solution_base import SolutionBase
 
 
 class Solution(SolutionBase):
+    @staticmethod
+    def solution(data):
+        houses = [(0, 0)]
+
+        for direction in data:
+            x, y = houses[-1]
+
+            if direction == "^":
+                y += 1
+            elif direction == "v":
+                y -= 1
+            elif direction == ">":
+                x += 1
+            elif direction == "<":
+                x -= 1
+
+            houses.append((x, y))
+
+        return houses
+
     def part1(self, data):
-        return
+        data = data[0]
+        data = [x for x in data]
+        houses = self.solution(data)
+        return len(set(houses))
 
     def part2(self, data):
-        return
+        data = data[0]
+        data = [x for x in data]
+
+        santa = data[::2]
+        robo_santa = data[1::2]
+
+        houses_santa = self.solution(santa)
+        houses_robo_santa = self.solution(robo_santa)
+
+        houses = houses_santa + houses_robo_santa
+        return len(set(houses))
